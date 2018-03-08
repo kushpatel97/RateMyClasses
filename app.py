89206a0466eb0a47e78c8fd798081c925e800de9
Kush Patel <kushp447@gmail.com>
2018-03-07 23:24:14 -0500

    Login Implementation

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, request, flash, url_for, session, g
 from flask_mysqldb import MySQL
 from flask_wtf import FlaskForm
-import json, requests
 
 
 
@@ -15,30 +14,73 @@ app.config['MYSQL_HOST'] = 'cs336-khp51-grader.creufxlr206t.us-east-2.rds.amazon
 app.config['MYSQL_USER'] = 'cs336khp51grader'
 app.config['MYSQL_PASSWORD'] = 'cs336khp51'
 app.config['MYSQL_DB'] = 'ratemyclasses'
-# app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #this config line returns queries we execute as dictionaries, default is to return as a tuple; ex. User Login
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #this config line returns queries we execute as dictionaries, default is to return as a tuple; ex. User Login
 
 db = MySQL(app)
 
 
 @app.route('/')
 def index():
+    return render_template('index.html')
 
-    cur = db.connection.cursor()
-
-    cur.execute('SELECT * FROM Users')
-    data = cur.fetchone()
-    db.connection.commit()
-    cur.close()
-
-    return render_template('index.html',data=data)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html',)
+    if request.method == 'POST':
+        email = str(request.form['email'])
+        password = str(request.form['password'])
 
-@app.route('/register', methods=['GET', 'POST'])
+        connection = db.connection
+        cur = connection.cursor()
+
+        result = cur.execute('SELECT * FROM Users WHERE Email=%s', [email])
+
+        if result > 0:
+            data = cur.fetchone()
+            datapass = data['Password']
+
+            if datapass == password:
+                cur.close()
+                return redirect(url_for('home'))
+            else:
+                error = 'Wrong Password!'
+                return render_template('login.html',error=error)
+        else:
+            cur.close()
+            error = 'No email found!'
+            return render_template('login.html',error=error)
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html',)
+    if request.method == 'POST':
+        username = str(request.form['username'])
+        email = str(request.form['email'])
+        password = str(request.form['password'])
+        # con_password = str(request.form['con_password'])
+
+        connection = db.connection
+        cur = connection.cursor()
+        result = cur.execute("SELECT * FROM Users WHERE Username=%s",[username])
+
+        if result > 0:
+            flash('Username already exists', 'danger')
+            return render_template('register.html')
+
+        cur.execute('INSERT INTO Users (Username, Email, Password) VALUES(%s, %s, %s)',(username, email,password))
+
+        db.connection.commit()
+
+        cur.close()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
