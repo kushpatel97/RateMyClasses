04a0d949d8f6b0e544572dfbc9e17facda8fa151
Kush Patel <kushp447@gmail.com>
2018-04-07 20:53:41 -0400

    User sessions completed

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, redirect, request, flash, url_for, session, g
 from flask_mysqldb import MySQL
+from functools import wraps
+import os
 
 
 
 app = Flask(__name__)
 
-
-
 #MySQL config
-app.secret_key = '123456789'
+app.secret_key = os.urandom(24)
 app.config['MYSQL_HOST'] = 'ratemyclass.cej3ioszzgpd.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_USER'] = 'ratemyclass'
 app.config['MYSQL_PASSWORD'] = 'ratemyclass'
@@ -16,6 +16,14 @@ app.config['MYSQL_DB'] = 'ratemyclass'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #this config line returns queries we execute as dictionaries, default is to return as a tuple; ex. User Login
 
 db = MySQL(app)
+
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
 
 
 @app.route('/')
@@ -26,6 +34,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        session.pop('user', None)
         username = str(request.form['username'])
         password = str(request.form['password'])
 
@@ -40,13 +49,16 @@ def login():
 
             if datapass == password:
                 cur.close()
+                session['user'] = username
                 return redirect(url_for('home'))
             else:
-                error = 'Wrong Password!'
+                error = 'Wrong Password! Try Again'
+                flash(error, 'danger')
                 return render_template('login.html',error=error)
         else:
             cur.close()
             error = 'No email found!'
+            flash(error, 'danger')
             return render_template('login.html',error=error)
     return render_template('login.html')
 
@@ -85,18 +97,30 @@ def register():
     return render_template('register.html', majorlist=majorlist)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash('You just logged out', 'success')
+    return redirect(url_for('login'))
+
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    majorlist = []
-    connection = db.connection
-    cur = connection.cursor()
-    cur.execute('SELECT * FROM majors;')
-    majorlist = cur.fetchall()
-    db.connection.commit()
-    cur.close()
-    return render_template('home.html',majorlist=majorlist)
+    if g.user:
+        majorlist = []
+        connection = db.connection
+        cur = connection.cursor()
+        cur.execute('SELECT * FROM majors;')
+        majorlist = cur.fetchall()
+        db.connection.commit()
+        cur.close()
+
+        test = g.user
+        return render_template('home.html', majorlist=majorlist, test=test)
+    else:
+        flash('Use the front door!','danger')
+        return redirect(url_for('login'))
 
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
