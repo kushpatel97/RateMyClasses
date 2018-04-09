fbbff42442bff20103c438f5789df7aa66fb08c0
Kush Patel <kushp447@gmail.com>
2018-04-08 23:02:19 -0400

    tuple script

---
+++ tuplescript.py
@@ -0,0 +1,6 @@
+from class_script import *
+from database_script import *
+
+
+
+query ="INSERT INTO `ratemyclass`.`Reviews` (`Class_id`, `Difficulty_rating`, `knowledge_gain_rating`, `review`, `Username`, `Review_year`, `Review_semester`) VALUES ('213', '4', '4', 'review text', 't7', '2018', 'Fall');"
